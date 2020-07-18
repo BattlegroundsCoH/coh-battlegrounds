607463a237db22cdb7b0d0f11930afb8c84858aa
RagnarTheEverchosen <adam.franc29@gmail.com>
2020-07-18 12:29:47 +0200

    Lobby list will now load lobbys from server. Base for lobby hosting layed down.

---
+++ coh2-battlegrounds/coh2-battlegrounds/ServerMessageHandler.cs
@@ -0,0 +1,58 @@
+﻿using Battlegrounds.Game.Battlegrounds;
+using Battlegrounds.Online.Services;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace BattlegroundsApp
+{
+    internal static class ServerMessageHandler
+    {
+        internal static LobbyHub hub = new LobbyHub();
+
+        private static ManagedLobby __LobbyInstance;
+
+        private static void OnPlayerEvent(ManagedLobbyPlayerEventType type, string from, string message)
+        {
+            switch (type)
+            {
+                case ManagedLobbyPlayerEventType.Join:
+                    {
+                        break;
+                    }
+                case ManagedLobbyPlayerEventType.Leave:
+                    {
+                        break;
+                    }
+                case ManagedLobbyPlayerEventType.Kicked:
+                    {
+                        break;
+                    }
+                case ManagedLobbyPlayerEventType.Message:
+                    {
+                        break;
+                    }
+                case ManagedLobbyPlayerEventType.Meta:
+                    {
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Something went wrong.");
+                        break;
+                    }
+            }
+        }
+
+        public static void OnServerResponse(ManagedLobbyStatus status, ManagedLobby resault)
+        {
+            if (status.Success)
+            {
+                __LobbyInstance = resault;
+
+                __LobbyInstance.OnPlayerEvent += OnPlayerEvent;
+            }
+        }
+    }
+}
