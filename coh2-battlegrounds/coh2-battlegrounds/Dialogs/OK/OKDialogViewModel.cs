50648218ce75e9724d1ad548181255850f5ea9b1
RagnarTheEverchosen <adam.franc29@gmail.com>
2021-01-05 13:42:53 +0100

    ok dialog (generic dialog for displaying stuff to user)

---
+++ coh2-battlegrounds/coh2-battlegrounds/Dialogs/OK/OKDialogViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using BattlegroundsApp.Dialogs.Service;
+using BattlegroundsApp.Utilities;
+using BattlegroundsApp.ViewModels;
+
+namespace BattlegroundsApp.Dialogs.OK {
+    public enum OKDialogResult {
+        OK,
+    }
+    class OKDialogViewModel : ViewModelBase, IDialogViewModelBase<OKDialogResult> {
+
+        public ICommand OKCommand { get; private set; }
+        public string Title { get; set; }
+        public string Message { get; set; }
+        public OKDialogResult DialogResult { get; set; }
+        public OKDialogResult DialogCloseDefault => OKDialogResult.OK;
+
+        public OKDialogViewModel(string title, string message) {
+
+            Title = title;
+            Message = message;
+            OKCommand = new RelayCommand<IDialogWindow>(OK);
+
+        }
+
+        private void OK(IDialogWindow window) {
+
+            CloseDialogWithResult(window, OKDialogResult.OK);
+
+        }
+
+        public void CloseDialogWithResult(IDialogWindow dialog, OKDialogResult result) {
+
+            DialogResult = result;
+
+            if (dialog != null) dialog.DialogResult = true;
+
+        }
+
+    }
+}
