6ab1508e05cd3bd8f097f2970d774f5526d94d44
JustCoDiEx <codiex1944@gmail.com>
2021-01-14 20:41:28 +0100

    Changed to self state.

--- coh2-battlegrounds/coh2-battlegrounds/Controls/Lobby/SelfState.cs
+++ coh2-battlegrounds/coh2-battlegrounds/Controls/Lobby/SelfState.cs
@@ -4,7 +4,7 @@ namespace BattlegroundsApp.Controls.Lobby {
     
     public class SelfState : BasicState {
 
-        public override bool IsCorrectState(LobbyControlContext context) => context.IsAI && context.IsHost || context.ClientID == BattlegroundsInstance.LocalSteamuser.ID;
+        public override bool IsCorrectState(LobbyControlContext context) => (context.IsAI && context.IsHost) || context.ClientID == BattlegroundsInstance.LocalSteamuser.ID;
 
         public override void SetStateIdentifier(ulong ownerID, bool isAI) { 
         }
