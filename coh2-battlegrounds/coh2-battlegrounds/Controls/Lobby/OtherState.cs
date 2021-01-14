af9fbd24466ada88dfecad9252e0e6eb112d9401
JustCoDiEx <codiex1944@gmail.com>
2021-01-14 20:50:36 +0100

    Maybe fix to "other" state.

--- coh2-battlegrounds/coh2-battlegrounds/Controls/Lobby/OtherState.cs
+++ coh2-battlegrounds/coh2-battlegrounds/Controls/Lobby/OtherState.cs
@@ -4,7 +4,7 @@ namespace BattlegroundsApp.Controls.Lobby {
 
     public class OtherState : BasicState {
 
-        public override bool IsCorrectState(LobbyControlContext context) => !context.IsHost;
+        public override bool IsCorrectState(LobbyControlContext context) => true;
 
         public override void SetStateIdentifier(ulong ownerID, bool isAI) { }
 
