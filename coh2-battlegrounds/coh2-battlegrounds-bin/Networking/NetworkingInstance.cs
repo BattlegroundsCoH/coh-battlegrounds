ffd209cc4ea8e23774b5fc6fbfdc7ca3377b8abb
CoDiEx <codiex1944@gmail.com>
2021-06-26 20:08:54 +0200

    The app will now try to connect to a local server before trying to connect to the official server.

--- coh2-battlegrounds/coh2-battlegrounds-bin/Networking/NetworkingInstance.cs
+++ coh2-battlegrounds/coh2-battlegrounds-bin/Networking/NetworkingInstance.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 
 using Battlegrounds.Networking.Communication;
 using Battlegrounds.Networking.Server;
@@ -33,10 +34,14 @@ namespace Battlegrounds.Networking {
                     var connection = TcpConnection.EstablishConnectionTo("192.168.1.107", 11000, 1);
                     BestAddress = connection.Ping() >= 0 ? "192.168.1.107" : "194.37.80.249";
                     connection.Shutdown();
-                } catch { }
+                } catch (Exception e) {
+                    Trace.WriteLine(e, nameof(NetworkingInstance));
+                    BestAddress = "194.37.80.249";
+                }
 #else
                 BestAddress = "194.37.80.249";
 #endif
+                Trace.WriteLine($"Using {BestAddress} as connection address", nameof(NetworkingInstance));
             }
             return BestAddress;
         }
