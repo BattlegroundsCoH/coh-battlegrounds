674e4c4fedaf4215dccfafa054e12c0232aa0723
Adam Franc <32552902+RagnarTheEverchosen@users.noreply.github.com>
2022-01-29 15:59:53 +0100

    Semi finished CompanyBuilderView
    Still couple of TODOs left

--- coh2-battlegrounds/coh2-battlegrounds/MVVM/Models/CompanyBrowserViewModel.cs
+++ coh2-battlegrounds/coh2-battlegrounds/MVVM/Models/CompanyBrowserViewModel.cs
@@ -1,10 +1,16 @@
 ﻿using Battlegrounds.Game.DataCompany;
+using Battlegrounds.Game.Gameplay;
 using Battlegrounds.Locale;
+using Battlegrounds.Modding;
+using BattlegroundsApp.Dialogs.CreateCompany;
+using BattlegroundsApp.Dialogs.RenameCopyDialog;
+using BattlegroundsApp.Dialogs.YesNo;
 using BattlegroundsApp.LocalData;
 using BattlegroundsApp.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -40,13 +46,15 @@ public class CompanyBrowserViewModel : IViewModel {
 
     public LocaleKey NameListViewHeader { get; }
 
-    public LocaleKey StrengthViewHeader { get; }
+    public LocaleKey StrengthListViewHeader { get; }
 
-    public LocaleKey TypeViewHeader { get; }
+    public LocaleKey TypeListViewHeader { get; }
 
-    public LocaleKey AllianceViewHeader { get; }
+    public LocaleKey AllianceListViewHeader { get; }
 
-    public LocaleKey CountryViewHeader { get; }
+    public LocaleKey CountryListViewHeader { get; }
+
+    public Company SelectedCompany { get; set; }
 
     public bool SingleInstanceOnly => true;
 
@@ -104,39 +112,95 @@ public class CompanyBrowserViewModel : IViewModel {
 
         // Define locales
         this.NameListViewHeader = new LocaleKey("CompanyView_Name");
-        this.StrengthViewHeader = new LocaleKey("CompanyView_Rating"); // TODO: Change the LOCALE string name to 'CompanyBrowserView_Strength' 
-        this.TypeViewHeader = new LocaleKey("CompanyView_Type");
-        this.AllianceViewHeader = new LocaleKey("CompanyView_Alliance");
-        this.CountryViewHeader = new LocaleKey("CompanyView_Country");
+        this.StrengthListViewHeader = new LocaleKey("CompanyView_Rating"); // TODO: Change the LOCALE string name to 'CompanyBrowserView_Strength' 
+        this.TypeListViewHeader = new LocaleKey("CompanyView_Type");
+        this.AllianceListViewHeader = new LocaleKey("CompanyView_Alliance");
+        this.CountryListViewHeader = new LocaleKey("CompanyView_Country");
 
     }
 
     public void CreateButton() {
 
+        ModGuid modGuid = ModManager.GetPackage("mod_bg").TuningGUID;
+        Trace.TraceWarning("There is currently no method of setting tuning pack. This should be fixed ASAP.");
+
+        if (CreateCompanyDialogViewModel.ShowCreateCompanyDialog(new LocaleKey("CompanyView_CreateCompanyDialog_Title"), out string companyName, out Faction companyFaction, out CompanyType companyType) 
+            is CreateCompanyDialogResult.Create) {
+
+            CompanyBuilderViewModel companyBuilder = new CompanyBuilderViewModel(companyName, companyFaction, companyType, modGuid);
+
+            App.ViewManager.UpdateDisplay(AppDisplayTarget.Right, companyBuilder);
+
+        }
+
     }
 
-    public void EditButton() { 
-    
+    public void EditButton() {
+
+        CompanyBuilderViewModel companyBuilder = new CompanyBuilderViewModel(SelectedCompany);
+
+        App.ViewManager.UpdateDisplay(AppDisplayTarget.Right, companyBuilder);
+
     }
 
+    // TODO: CompanyTemplate.cs is broken
     public void RenameButton() {
+
+        if (RenameCopyDialogViewModel.ShowRenameDialog(new LocaleKey("CompanyView_RenameCopyDialog_Rename_Title"), out string companyName)
+            is RenameCopyDialogResult.Rename) {
+
+            var builder = new CompanyBuilder();
+            builder.CloneCompany(SelectedCompany, companyName, CompanyAvailabilityType.MultiplayerOnly).Commit();
+
+            PlayerCompanies.DeleteCompany(SelectedCompany);
+            PlayerCompanies.SaveCompany(builder.Result);
+
+            UpdateCompanyList();
+
+        }
 
     }
 
     public void DeleteButton() { 
+
+        if (YesNoDialogViewModel.ShowYesNoDialog(new LocaleKey("CompanyView_YesNoDialog_Delete_Company_Title"), new LocaleKey("CompanyView_YesNoDialog_Delete_Company_Message"))
+            is YesNoDialogResult.Confirm) {
+
+            PlayerCompanies.DeleteCompany(SelectedCompany);
+
+            UpdateCompanyList();
+
+        }
     
     }
 
+    // TODO: CompanyTemplate.cs is broken
     public void CopyButton() {
+
+        if (RenameCopyDialogViewModel.ShowCopyDialog(new LocaleKey("CompanyView_RenameCopyDialog_Copy_Title"), out string companyName)
+            is RenameCopyDialogResult.Copy) {
+
+            var builder = new CompanyBuilder();
+            builder.CloneCompany(SelectedCompany, companyName, CompanyAvailabilityType.MultiplayerOnly).Commit();
+
+            PlayerCompanies.SaveCompany(builder.Result);
+
+            UpdateCompanyList();
+
+        }
 
     }
 
     public void ExportButton() { 
     
+        // TODO
+
     }
 
     public void ImportButton() { 
     
+        // TODO
+
     }
 
     public void EditCompany(object sender, MouseButtonEventArgs args)
