34059962675b5645f0afddf213f01fccca0a9c2d
Adam Franc <32552902+RagnarTheEverchosen@users.noreply.github.com>
2022-03-17 21:09:41 +0100

    AvailableSquadSlot

---
+++ coh2-battlegrounds/coh2-battlegrounds/CompanyEditor/MVVM/Models/AvailableSquadViewModel.cs
@@ -0,0 +1,54 @@
+﻿using Battlegrounds.Game.Database;
+using BattlegroundsApp.MVVM;
+using BattlegroundsApp.Resources;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace BattlegroundsApp.CompanyEditor.MVVM.Models;
+
+public delegate void AvailableSquadViewModelEvent(object sender, AvailableSquadViewModel availableSquadViewModel);
+
+public class AvailableSquadViewModel : IViewModel {
+
+    public string SquadName { get; }
+
+    public ImageSource SquadSymbol { get; }
+
+    public Blueprint Blueprint { get; }
+
+    public AvailableSquadViewModelEvent AddClick { get; }
+
+    public AvailableSquadViewModelEvent Move { get; }
+
+    public bool CanAdd { get; set; }
+
+    public bool SingleInstanceOnly => false; // This will allow us to override
+
+    public AvailableSquadViewModel(Blueprint bp, AvailableSquadViewModelEvent onAdd, AvailableSquadViewModelEvent onMove) {
+
+        if (bp is SquadBlueprint sbp) {
+
+            this.SquadName = GameLocale.GetString(sbp.UI.ScreenName);
+            this.SquadSymbol = App.ResourceHandler.GetIcon("unit_symbols", sbp.UI.Symbol);
+            this.Blueprint = sbp;
+
+        } else {
+
+            throw new ArgumentException($"Invalid blueprint type '{bp.GetType().Name}'.", nameof(bp));
+
+        }
+
+        // Set events
+        this.AddClick = onAdd;
+        this.Move = onMove;
+
+    }
+
+    public bool UnloadViewModel() => true;
+
+}
---
+++ coh2-battlegrounds/coh2-battlegrounds/CompanyEditor/MVVM/Views/AvailableSquadView.xaml.cs
@@ -0,0 +1,41 @@
+﻿using BattlegroundsApp.CompanyEditor.MVVM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BattlegroundsApp.CompanyEditor.MVVM.Views;
+
+/// <summary>
+/// Interaction logic for AvailableSquadView.xaml
+/// </summary>
+public partial class AvailableSquadView : UserControl {
+
+    public AvailableSquadView() {
+        InitializeComponent();
+    }
+
+    protected override void OnMouseMove(MouseEventArgs e) {
+        base.OnMouseMove(e);
+        if (this.DataContext is AvailableSquadViewModel vm) {
+            vm.Move?.Invoke(this, vm);
+        }
+    }
+
+    private void AddButton_Click(object sender, RoutedEventArgs e) { 
+        if (this.DataContext is AvailableSquadViewModel vm) {
+            vm.AddClick?.Invoke(this, vm);
+        }
+    }
+
+}
