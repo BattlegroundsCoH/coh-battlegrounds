b355ad1b9de10285fde91218b3b660ea7ae8a0b3
Adam Franc <32552902+RagnarTheEverchosen@users.noreply.github.com>
2022-03-19 12:18:34 +0100

    AbilitySlot added

---
+++ coh2-battlegrounds/coh2-battlegrounds/CompanyEditor/MVVM/Models/AbilitySlotViewModel.cs
@@ -0,0 +1,53 @@
+﻿using Battlegrounds.Game.Database.Extensions;
+using Battlegrounds.Game.Gameplay;
+using BattlegroundsApp.MVVM;
+using BattlegroundsApp.Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattlegroundsApp.CompanyEditor.MVVM.Models;
+
+public delegate void AbilitySlotViewModelEvent(object sender, AbilitySlotViewModel slotViewModel);
+
+public class AbilitySlotViewModel : IViewModel {
+
+    public string AbilityName { get; set; }
+
+    public string AbilityIcon { get; set; }
+
+    public string AbilitySymbol { get; set; }
+
+    public CostExtension AbilityCost { get; set; }
+
+    public Ability AbilityInstance { get; }
+
+    public AbilitySlotViewModelEvent Click { get; }
+
+    public AbilitySlotViewModelEvent RemoveClick { get; }
+
+    public bool SingleInstanceOnly => false; // This will allow us to override
+
+    public AbilitySlotViewModel(Ability ability, AbilitySlotViewModelEvent onClick, AbilitySlotViewModelEvent onRemove) {
+
+        // Set ability instance
+        this.AbilityInstance = ability;
+
+        // Set events
+        this.Click = onClick;
+        this.RemoveClick = onRemove;
+
+        // Set data
+        this.AbilityName = GameLocale.GetString(this.AbilityInstance.ABP.UI.ScreenName);
+        this.AbilityCost = this.AbilityInstance.ABP.Cost;
+
+        this.AbilityIcon = this.AbilityInstance.ABP.UI.Icon;
+        this.AbilitySymbol = this.AbilityInstance.ABP.UI.Symbol;
+
+    }
+
+    public bool UnloadViewModel() => true;
+
+}
---
+++ coh2-battlegrounds/coh2-battlegrounds/CompanyEditor/MVVM/Views/AbilitySlotView.xaml.cs
@@ -0,0 +1,55 @@
+﻿using BattlegroundsApp.CompanyEditor.MVVM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BattlegroundsApp.CompanyEditor.MVVM.Views;
+/// <summary>
+/// Interaction logic for AbilitySlotView.xaml
+/// </summary>
+public partial class AbilitySlotView : UserControl {
+
+    public static readonly Brush VIEW_DEFAULT = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#334252"));
+    public static readonly Brush VIEW_HOVER = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#536375"));
+
+    public AbilitySlotView() {
+        InitializeComponent();
+    }
+
+    protected override void OnMouseEnter(MouseEventArgs e) {
+        base.OnMouseEnter(e);
+        this.Background = VIEW_HOVER;
+        this.RemoveButton.Visibility = Visibility.Visible;
+    }
+
+    protected override void OnMouseLeave(MouseEventArgs e) {
+        base.OnMouseLeave(e);
+        this.Background = VIEW_DEFAULT;
+        this.RemoveButton.Visibility = Visibility.Collapsed;
+    }
+
+    protected override void OnMouseDown(MouseButtonEventArgs e) {
+        base.OnMouseDown(e);
+        if (this.DataContext is AbilitySlotViewModel vm) {
+            vm.Click?.Invoke(this, vm);
+        }
+    }
+
+    private void RemoveButton_Click(object sender, RoutedEventArgs e) {
+        if (this.DataContext is AbilitySlotViewModel vm) {
+            vm.RemoveClick?.Invoke(this, vm);
+        }
+    }
+
+}
