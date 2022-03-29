faa85227d62924c1243d23aaa885e412c1fa9e56
Adam Franc <32552902+RagnarTheEverchosen@users.noreply.github.com>
2022-03-29 12:52:18 +0200

    LobbyJoinDialog base

---
+++ coh2-battlegrounds/coh2-battlegrounds/Modals/Dialogs/MVVM/Models/LobbyJoinDialogViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattlegroundsApp.Modals.Dialogs.MVVM.Models;
+
+public class LobbyJoinDialogViewModel {
+
+    public LobbyJoinDialogViewModel() {
+
+
+
+    }
+
+}
