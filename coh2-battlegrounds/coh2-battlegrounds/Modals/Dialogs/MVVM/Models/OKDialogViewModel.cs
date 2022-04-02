a93adfce72e21d5ab0187f71eba9fe60627845ce
Adam Franc <32552902+RagnarTheEverchosen@users.noreply.github.com>
2022-04-02 19:08:50 +0200

    OKDialog added

---
+++ coh2-battlegrounds/coh2-battlegrounds/Modals/Dialogs/MVVM/Models/OKDialogViewModel.cs
@@ -0,0 +1,42 @@
+﻿using BattlegroundsApp.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace BattlegroundsApp.Modals.Dialogs.MVVM.Models;
+
+public class OKDialogViewModel {
+
+    public string DialogTitle { get; set; }
+
+    public string DialogMessage { get; set; }
+
+    public ICommand OKCommand { get; }
+
+    public OKDialogViewModel(Action<OKDialogViewModel, ModalDialogResult> resultCallback, string title, string message) {
+
+        this.OKCommand = new RelayCommand(() => resultCallback?.Invoke(this, ModalDialogResult.Confirm));
+
+        this.DialogTitle = title;
+        this.DialogMessage = message;
+
+    }
+
+    public static void ShowModal(ModalControl control, Action<OKDialogViewModel, ModalDialogResult> resultCallback,
+                                 string title, string message) {
+
+        // Create dialog view model
+        OKDialogViewModel dialog = new((vm, result) => {
+            resultCallback(vm, result);
+            control.CloseModal();
+        }, title, message);
+
+        control.ModalMaskBehaviour = ModalBackgroundBehaviour.ExitWhenClicked;
+        control.ShowModal(dialog);
+
+    }
+
+}
