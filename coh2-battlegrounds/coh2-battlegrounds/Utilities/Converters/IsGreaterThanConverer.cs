a0075b1ca72241bedd0ada64154530f46f45f676
Adam Franc <32552902+RagnarTheEverchosen@users.noreply.github.com>
2022-07-04 16:43:12 +0200

    IsLessThan & IsGreaterThan convertors

---
+++ coh2-battlegrounds/coh2-battlegrounds/Utilities/Converters/IsGreaterThanConverer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace BattlegroundsApp.Utilities.Converters;
+public class IsGreaterThanConverer : IValueConverter {
+
+    public static readonly IValueConverter Instance = new IsGreaterThanConverer();
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+
+        double doubleValue = System.Convert.ToDouble(value);
+        double compareToValue = System.Convert.ToDouble(parameter);
+
+        return doubleValue > compareToValue;
+
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+
+        throw new NotImplementedException();
+    
+    }
+}
+
