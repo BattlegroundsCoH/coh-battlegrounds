481d0dd40cf2bacc87d629e6e7f90b189c026901
Adam Franc <32552902+RagnarTheEverchosen@users.noreply.github.com>
2022-09-16 23:17:14 +0200

    get latest release from github api

---
+++ coh2-battlegrounds/coh2-battlegrounds-bin/Update/Release.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Battlegrounds.Update;
+
+public class Release {
+
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("tag_name")]
+    public string TagName { get; set; }
+
+    [JsonPropertyName("published_at")]
+    public DateTime PublishedAt { get; set; }
+
+    [JsonPropertyName("assets")]
+    public List<Asset> Assets { get; set; }
+
+}
+
+public class Asset {
+
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("browser_download_url")]
+    public string InstallerDownloadUrl { get; set; }
+
+}
---
+++ coh2-battlegrounds/coh2-battlegrounds-bin/Update/Update.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Reflection;
+using System;
+
+namespace Battlegrounds.Update;
+
+public static class Update {
+
+	// 1. Check for new version
+	// 2. If new version => download newest version
+	// 3. Close application
+	// 4. Install newest version
+
+	private static readonly HttpClient _httpClient = new HttpClient();
+
+	private static async Task<Release> ProcessLatestRelease() {
+
+		_httpClient.DefaultRequestHeaders.Accept.Clear();
+		_httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+		_httpClient.DefaultRequestHeaders.Add("User-Agent", "Battlegrounds Mod Launcher");
+
+		var streamTask = await _httpClient.GetStreamAsync("https://api.github.com/repos/JustCodiex/coh2-battlegrounds/releases/latest");
+        var release = JsonSerializer.Deserialize<Release>(streamTask);
+
+		return release;
+
+	}
+
+}
