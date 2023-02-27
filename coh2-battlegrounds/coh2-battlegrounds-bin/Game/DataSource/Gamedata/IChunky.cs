fba3c032592ff4b760bd830304697e6c851ff962
JustCodiex <codiex1944@gmail.com>
2023-02-27 20:28:36 +0100

    Implemented chunky reader.

---
+++ coh2-battlegrounds/coh2-battlegrounds-bin/Game/DataSource/Gamedata/IChunky.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+
+namespace Battlegrounds.Game.DataSource.Gamedata;
+
+/// <summary>
+/// 
+/// </summary>
+public interface IChunky {
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="reader"></param>
+    /// <returns></returns>
+    bool Load(BinaryReader reader);
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="jsonOutputFile"></param>
+    void DumpJson(string jsonOutputFile);
+
+}
--- coh2-battlegrounds/coh2-battlegrounds-bin/Game/DataSource/Playback/CoH3/CoH3Playback.cs
+++ coh2-battlegrounds/coh2-battlegrounds-bin/Game/DataSource/Playback/CoH3/CoH3Playback.cs
@@ -2,8 +2,12 @@
 using System.IO;
 using System.Text;
 
+using Battlegrounds.Functional;
+using Battlegrounds.Game.DataSource.Gamedata;
+using Battlegrounds.Game.DataSource.Gamedata.CoH3;
 using Battlegrounds.Game.Gameplay;
 using Battlegrounds.Logging;
+using Battlegrounds.Util;
 
 namespace Battlegrounds.Game.DataSource.Playback.CoH3;
 
@@ -81,6 +85,34 @@ public class CoH3Playback : IPlayback {
             return false;
         }
 
+        // Read info chunky
+        IChunky infoChunky = new Chunky();
+        if (!infoChunky.Load(br)) {
+            return false;
+        }
+
+        infoChunky.DumpJson("playback_info.json");
+
+        // Read replay chunky
+        IChunky playbackChunky = new Chunky();
+        if (!playbackChunky.Load(br)) {
+            return false;
+        }
+
+        playbackChunky.DumpJson("playback_playback.json");
+
+        // Get playback data
+        byte[] replaydata = br.ReadToEnd();
+
+        // Dump playback data
+        File.WriteAllBytes("playback_raw.bin", replaydata);
+
+        // TODO: Parse chunky raw binary
+        if (!ParsePlaybackData(replaydata)) {
+            logger.Info("Failed reading playback data: " + this.playbackFile);
+            return false;
+        }
+
         return true;
 
     }
@@ -114,6 +146,77 @@ public class CoH3Playback : IPlayback {
         this.header = new(version, gameVersion, dateBuilder.ToString());
 
         // Return OK
+        return true;
+
+    }
+
+    private const int PlaybackDataType_Tick = 0;
+    private const int PlaybackDataType_Chat = 1;
+
+    private bool ParsePlaybackData(byte[] data) {
+        
+        // Open parsers
+        using MemoryStream ms = new MemoryStream(data);
+        using BinaryReader reader = new BinaryReader(ms);
+
+        // Read until EOF
+        while (ms.Position < ms.Length) {
+
+            uint dataType = reader.ReadUInt32();
+            uint dataSize = reader.ReadUInt32();
+
+            if (dataSize == 0)
+                continue;
+
+            if (dataType == PlaybackDataType_Tick) {
+
+                // Skip one byte
+                logger.Debug("First byte of tick: " + reader.ReadByte());
+
+                // Get tick index
+                uint index = reader.ReadUInt32();
+                
+                // Calculate the timestamp
+                TimeSpan timestamp = new TimeSpan((long)(10000000.0 * index / 8.0));
+
+                // Skip next four bytes
+                logger.Debug("Skipping next four bytes: [" + string.Join("; ", reader.ReadBytes(4).Map(x => x.ToString("X2"))) + "]");
+
+                // Read tick events
+                uint events = reader.ReadUInt32();
+
+                // Read event bundle
+                for (uint i = 0; i < events; i++) {
+
+                    // Skip first 8 bytes
+                    logger.Debug("Skipping next four bytes: [" + string.Join("; ", reader.ReadBytes(8).Map(x => x.ToString("X2"))) + "]");
+
+                    uint count = reader.ReadUInt32();
+
+                    //logger.Debug("Skipping next byte: [" + string.Join("; ", reader.ReadBytes(1).Map(x => x.ToString("X2"))) + "]");
+
+                    // Read all
+                    for (int j = 0; j < count; j++) {
+
+                        ushort len = reader.ReadUInt16();
+                        if (len != 0) {
+
+                            reader.ReadBytes(len);
+
+                        }
+
+                    }
+
+                }
+
+            } else if (dataType == PlaybackDataType_Chat) {
+
+            } else {
+                logger.Error("Failed reading replay file with invalid dataType: " + dataType);
+            }
+
+        }
+
         return true;
 
     }
